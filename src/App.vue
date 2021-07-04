21da335e4b34cc7912349f014a9d842360b57573
tranthiensonuet <tranthienson1999@gmail.com>
2021-07-04 22:25:17 +0700

    tạo project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <home-header></home-header>
+
+    <home-content></home-content>
+  </v-app>
+</template>
+
+<script>
+import HomeContent from './components/HomeContent.vue';
+import HomeHeader from './components/HomeHeader.vue';
+
+
+export default {
+  name: 'App',
+  components: {
+    HomeHeader,
+    HomeContent
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/HomeHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app-bar app fixed>
+    <a href="" class="avatar"></a>
+    <v-spacer></v-spacer>
+    <v-btn v-for="header in listHeader" :key="header">
+      {{ header }}
+    </v-btn>
+    <v-spacer></v-spacer>
+    <v-btn>Đăng nhập</v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "home-header",
+  data: () => ({
+    listHeader: ["Giới thiệu", "Hồ sơ", "Liên hệ"],
+  }),
+};
+</script>
+<style>
+.avatar {
+  background: url("../assets/logo-st.png") center no-repeat;
+  height: 55px;
+  width: 150px;
+  background-size: 150%;
+  cursor: pointer;
+}
+</style>
